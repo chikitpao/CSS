dfe002fc8032bf0464b4167332be75a7f6f3f7c8
chikitpao <118132625+chikitpao@users.noreply.github.com>
2022-11-16 19:36:32 +0100

    Initial version

---
+++ calculate_sum_subsets.py
@@ -0,0 +1,342 @@
+# Count / calculate number of sums of subsets modulo d 
+# Problem related to youtube video "Olympiad level counting - How many subsets of {1,…,2000} have a sum divisible by 5"
+#  by 3Blue1Brown (url: https://www.youtube.com/watch?v=bOXCLR3Wric)
+from itertools import chain, combinations
+#import numpy as np
+import sympy as sp
+#from sympy import Symbol 
+from sympy import symbols
+import time
+
+from sympy.interactive.printing import init_printing
+
+
+## generate combinations and count
+# Source: https://docs.python.org/3/library/itertools.html#itertools-recipes
+def powerset(iterable):
+    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+
+
+def calculate_sum_mod(l, div):
+    result = 0
+    for e in l:
+        result = (result + e) % div
+    return result
+
+
+def try_result(n, div):
+    myList = [i for i in range(1, n + 1)]
+
+    #print(myList)
+    myPowerSetList = list(powerset(myList))
+    mod = [0] * div
+    for e in myPowerSetList:
+        r = calculate_sum_mod(e, div)
+        mod[r] = mod[r] + 1
+    return mod
+
+
+# ## calculate with matrix (numpy)
+# def create_matrix_from_solution_vector_np(v):
+#     dim = np.shape(v)[0] # 0 -> rows, 1 -> cols
+#     M = np.empty((dim, dim), dtype=int)
+#     for j in range(0, dim):
+#         for k in range(0, dim):
+#             M[(j + k) % dim][j] = v[k]
+#     return M
+
+            
+# def generate_solution_vectors_np(div):
+#     result = []
+    
+#     # Step 1: Create matrix for 0 elements (= Identity matrix)
+#     M = np.eye(div, dtype=int)
+    
+#     # Step 2: Create vector for different values (needs new matrix from last value)
+#     element_vector = np.zeros((div, 1), dtype=int)
+#     element_vector[0][0] = 1
+#     for i in range(1, div + 1):
+#         r = i % div
+#         element_vector[r][0] += 1
+#         # N = M * element_vector
+#         # v2 = np.multiply(M, element_vector)
+#         v2 = np.matmul(M, element_vector)
+#         result.append(v2)
+#         element_vector[r][0] -= 1        
+#         M = create_matrix_from_solution_vector_np(v2)
+#     return result
+# ##
+
+
+## calculate with matrix (sympy)
+def create_matrix_from_solution_vector_sp(v):
+    dim = v.shape[0] # 0 -> rows, 1 -> cols
+    M = sp.zeros(dim, dim)
+    for j in range(0, dim):
+        for k in range(0, dim):
+            M[(j + k) % dim, j] = v[k]
+    return M
+
+            
+def generate_solution_vectors_sp(div):
+    result = []
+    
+    # Step 1: Create matrix for 0 elements (= Identity matrix)
+    M = sp.eye(div)
+    
+    # Step 2: Create vector for different values (needs new matrix from last value)
+    element_vector = sp.zeros(div, 1)
+    element_vector[0, 0] = 1
+    for i in range(1, div + 1):
+        r = i % div
+        element_vector[r, 0] += 1
+        v2 = M * element_vector
+        result.append(v2)
+        element_vector[r, 0] -= 1        
+        M = create_matrix_from_solution_vector_sp(v2)
+    return result
+
+
+def calcluate_sum_subsets_linear(n, div):
+    if n < 0 or div < 2:
+        raise ValueError("n must be >= 0 and div must be >=2!")
+
+    v = sp.Matrix(div, 1, lambda row, col: 1 if row == 0 else 0)
+    
+    if n <= 0:
+        return v
+    
+    remainder = n % div
+    quotient = n // div
+
+    r = generate_solution_vectors_sp(div)
+    if quotient == 0:
+        return r[remainder-1]
+
+    M = sp.eye(div, div)
+
+    # Normally we shall handle full chunks first and then remainder.
+    # For besser program flow, order of handlings is swapped.
+    if remainder != 0:
+        v = M * r[remainder-1]
+        M = create_matrix_from_solution_vector_sp(v)
+
+    r_last = r[div-1]
+    for i in range(1, quotient + 1):
+        v = M * r_last
+        M = create_matrix_from_solution_vector_sp(v)
+
+    return v
+
+
+def calcluate_sum_subsets_logarithmic(n, div):
+    if n < 0 or div < 2:
+        raise ValueError("n must be >= 0 and div must be >=2!")
+
+    v = sp.Matrix(div, 1, lambda row, col: 1 if row == 0 else 0)
+    
+    if n <= 0:
+        return v
+    
+    remainder = n % div
+    quotient = n // div
+
+    r = generate_solution_vectors_sp(div)
+    if quotient == 0:
+        return r[remainder-1]
+
+    M = sp.eye(div, div)
+
+    # Normally we shall handle full chunks first then remainder.
+    # For besser program flow order of handlings is swapped.
+    if remainder != 0:
+        v = M * r[remainder-1]
+        M = create_matrix_from_solution_vector_sp(v)
+
+    temp_v = r[div-1]
+    temp_quotient = quotient
+    temp_M = sp.eye(div, div)
+    is_lsb_set = False
+    while temp_quotient > 0:
+        is_lsb_set = temp_quotient & 1
+        temp_v = temp_M * temp_v
+        if is_lsb_set:
+            M = M * temp_v
+            M = create_matrix_from_solution_vector_sp(M)
+        temp_M = create_matrix_from_solution_vector_sp(temp_v)
+        temp_quotient = temp_quotient // 2
+
+    return M[:, 0]
+
+
+def calcluate_sum_subsets_constant(n, div): # Use formulas
+    if n < 0 or div < 2:
+        raise ValueError("n must be >= 0 and div must be >=2!")
+
+    # REMARK CKP: We can't handle all divisors here! 
+    v = sp.Matrix(div, 1, lambda row, col: 1 if row == 0 else 0)
+    if n <= 0:
+        return v
+
+    remainder = n % div
+    temp_n = n - remainder
+    
+    if div == 3:
+        if temp_n > 0:
+            c3_0 = lambda x: (2**x + 2 * 2**(x//3)) // 3
+            c3_1 = lambda x: (2**x - 2**(x//3)) // 3
+            c_0 = c3_0(temp_n)
+            c_1 = c3_1(temp_n)
+            v = sp.Matrix(div, 1, lambda row, col: c_0 if row == 0 else c_1)
+        # else v remains the same
+    elif div == 5:
+        if temp_n > 0:
+            c5_0 = lambda x: (2**x + 4 * 2**(x//5)) // 5
+            c5_1 = lambda x: (2**x - 2**(x//5)) // 5
+            c_0 = c5_0(temp_n)
+            c_1 = c5_1(temp_n)
+            v = sp.Matrix(div, 1, lambda row, col: c_0 if row == 0 else c_1)
+        # else v remains the same
+    elif div == 6:
+        if temp_n > 0:
+            c6_0 = lambda x: (2**x + 2 * 4**(x//6)) // 6
+            c6_1 = lambda x: (2**x - 4**(x//6)) // 6
+            c_0 = c6_0(temp_n)
+            c_1 = c6_1(temp_n)
+            v = sp.Matrix(div, 1, lambda row, col: c_0 if row % 3 == 0 else c_1)
+        # else v remains the same
+    elif div == 7:
+        if temp_n > 0:
+            c7_0 = lambda x: (2**x + 6 * 2**(x//5)) // 7
+            c7_1 = lambda x: (2**x - 2**(x//5)) // 7
+            c_0 = c7_0(temp_n)
+            c_1 = c7_1(temp_n)
+            v = sp.Matrix(div, 1, lambda row, col: c_0 if row == 0 else c_1)
+        # else v remains the same
+    elif div == 9:
+        if temp_n > 0:
+            c9_0 = lambda x: (2**x + 2 * 2**(x//9) + 2* 2**(x//9 - 1) * (2 * 4**(x//9) + 4)) // 9
+            c9_1 = lambda x: (2**x + 2 * 2**(x//9) - 2**(x//9 - 1) * (2 * 4**(x//9) + 4)) // 9
+            c9_3 = lambda x: (2**x - 7 * 2**(x//9) + 2 * 2**(x//9 - 1) * (2 * 4**(x//9) + 4)) // 9
+            c_0 = c9_0(temp_n)
+            c_1 = c9_1(temp_n)
+            c_3 = c9_3(temp_n)
+            for i in range(0, div):
+                if i == 0:
+                    v[i, 0] = c_0
+                elif i % 3 == 0:
+                    v[i, 0] = c_3
+                else:
+                    v[i, 0] = c_1
+        # else v remains the same
+    else:
+        if div >= 2 and power_of_two(div):
+            if temp_n > 0:
+                c2n_0 = lambda x: 2**temp_n // div
+                c_0 = c2n_0(temp_n)
+                v = sp.Matrix(div, 1, lambda row, col: c_0)
+            # else v remains the same
+        else:
+            raise ValueError(f"div={div} is not supported by calcluate_sum_subsets_constant")
+
+    if remainder == 0:
+        return v
+
+    M = create_matrix_from_solution_vector_sp(v)
+    r = generate_solution_vectors_sp(div)
+    v = M * r[remainder-1]
+    return v
+
+ # Source for "power of two": https://stackoverflow.com/questions/57025836/how-to-check-if-a-given-number-is-a-power-of-two
+def power_of_two(x):
+    return (x & (x-1) == 0) and x > 0
+##
+
+
+## test try_result
+def test_try_result():
+    for div in range(2, 21):
+        for i in range (1, div + 1):
+            print(f"i: {i}, div: {div}, try_result: {try_result(i, div)}")
+
+
+# ## test numpy functions
+# def test_numpy():
+#     c6_0 = lambda x: (2**x + 2 * 4**(x//6)) // 6
+#     c6_1 = lambda x: (2**x - 4**(x//6)) // 6
+#     div = 6
+#     r6 = generate_solution_vectors_np(div)
+#     print(r6)
+#     v6 = r6[div-1]
+#     n = div
+#     print(f"6: v6: {v6} v6[0]-v6[1]: {v6[0]-v6[1]} n: {n} c6_0(n): {c6_0(n)} c6_1(n) {c6_1(n)}")
+#     for i in range(2, 6):
+#         M = create_matrix_from_solution_vector_np(v6)
+#         v6 = np.matmul(M, r6[div-1])
+#         n = div * i
+#         print(f"6: i {i} finished: v6: {v6} v6[0][0]-v6[1][0]: {v6[0][0]-v6[1][0]} n: {n} c6_0(n): {c6_0(n)} c6_1(n): {c6_1(n)}")    
+
+
+## test sympy functions
+def test_sympy():
+    c6_0 = lambda x: (2**x + 2 * 4**(x//6)) // 6
+    c6_1 = lambda x: (2**x - 4**(x//6)) // 6
+    div = 6
+    r6 = generate_solution_vectors_sp(div)
+    print(r6)
+    v6 = r6[div-1]
+    n = div
+    print(f"6: v6: {v6} v6[0]-v6[1]: {v6[0]-v6[1]} n: {n} c6_0(n): {c6_0(n)} c6_1(n) {c6_1(n)}")
+    for i in range(2, 12):
+        M = create_matrix_from_solution_vector_sp(v6)
+        v6 = M * v6  # was r6[div-1]
+        n = 2 * n
+        print(f"6: i {i} finished: v6: {v6} v6[0,0]-v6[1,0]: {v6[0,0]-v6[1,0]} n: {n} c6_0(n): {c6_0(n)} c6_1(n): {c6_1(n)}")    
+
+
+def test_power_of_two():
+    t = 2**2000
+    print(t, power_of_two(t))
+    t += 1
+    print(t, power_of_two(t))
+
+def test(n, div):
+    print(f"### n={n} div={div}")
+    
+    t = time.process_time()
+    r_log = calcluate_sum_subsets_logarithmic(n, div)
+    time_log = time.process_time() - t
+
+    t = time.process_time()
+    r_lin = calcluate_sum_subsets_linear(n, div)
+    time_lin = time.process_time() - t
+    
+    r = r_log - r_lin
+    print(f"result logarithm={r_lin}, elapsed={time_log} difference with linear={r}, elapsed={time_lin}")
+
+    try:
+        t = time.process_time()
+        r_const = calcluate_sum_subsets_constant(n, div)
+        time_con = time.process_time() - t
+        r = r_log - r_const
+        print(f"result logarithm={r_lin}, elapsed={time_log} difference with constant={r}, elapsed={time_con}")
+    except:
+        pass
+
+
+def main():
+    init_printing(use_unicode=False, wrap_line=False, num_columns=300)
+    #test_numpy()
+    #test_sympy()
+
+    #div = 9
+    #for i in range (0, 20):
+    #    test(i, div)
+    
+    test(2003, 9)
+
+
+if __name__ == '__main__':
+    main()
---
+++ construct_sum_subsets_formulars.py
@@ -0,0 +1,278 @@
+# Construct formulars for counting sum of subsets modulo div
+# Problem related to youtube video "Olympiad level counting - How many subsets of {1,…,2000} have a sum divisible by 5"
+#  by 3Blue1Brown (url: https://www.youtube.com/watch?v=bOXCLR3Wric)
+
+import sympy as sp
+#from sympy import Symbol 
+from sympy import symbols
+
+from sympy.interactive.printing import init_printing
+
+from calculate_sum_subsets import *
+
+class KeySymbol:
+    def __init__(self, symbol, index):
+        self.sym = symbol
+        self.members = [symbol]
+        self.index = index
+
+    def add_symbol(self, symbol):
+        self.members.append(symbol)
+    
+    def __str__(self):
+        return f"KeySymbol(members: {self.members}, index: {self.index})"
+
+    def __repr__(self):
+        return f"KeySymbol(members={self.members}, index={self.index})"
+
+
+class SymbolTable:
+    def __init__(self, div):
+        self.div = div
+        self.sym_c = symbols('c:' + str(div))
+        self.lookup_v_s = {} # value -> symbol
+        self.key_symbols = {} # key symbol -> KeySymbol objects
+        self.sym_c2 = list(self.sym_c)
+        self.r_div = generate_solution_vectors_sp(div)
+        self.v_div = self.r_div[div-1]
+        self.replace_symbols()
+
+    def replace_symbols(self):
+        for j in range(0, self.div):
+            key = self.v_div[j, 0]
+            if key in self.lookup_v_s:
+                self.sym_c2[j] = self.lookup_v_s[key]
+                self.key_symbols[self.sym_c2[j]].add_symbol(self.sym_c[j])
+            else:
+                self.lookup_v_s[key] = self.sym_c[j]
+                self.key_symbols[self.sym_c[j]] = KeySymbol(self.sym_c[j], j)
+
+    def get_v_div(self):
+        return self.v_div
+
+    def return_symbol_matrix(self):
+        M = sp.zeros(self.div, self.div)
+        for j in range(0, self.div):
+            for k in range(0, self.div):
+                M[(j + k) % self.div, j] = self.sym_c2[k]
+        return M    
+
+    def calculate_result(self, v_div_diff):
+        print("## SymbolTable.key_symbols: ", self.key_symbols, "len:", len(self.key_symbols))
+        if len(self.key_symbols) == 1:
+            symbol0 = list(self.key_symbols.keys())[0]
+            n = self.div
+            print(f"# Apply rule of \"power of two\" (#1). Formular: {symbol0} = 2**n // d")
+            print(f"n = {n} => {symbol0} = {2**n // self.div}")
+        elif len(self.key_symbols) == 2 or len(self.key_symbols) == 3:
+            key_symbols_keys = list(self.key_symbols)
+            diff = None
+            index_of_second_symbol_in_diff = None
+            for i in range(0, self.div):
+                free_symbols = v_div_diff[i, 0].free_symbols
+                if len(free_symbols) == 2:
+                    symbol_list = list(free_symbols)
+                    if symbol_list[0] == key_symbols_keys[0]: # c_0 minus another c_i
+                        diff = symbol_list[0] - symbol_list[1]
+                        index_of_second_symbol_in_diff = self.key_symbols[symbol_list[1]].index
+                    else:
+                        diff = symbol_list[1] - symbol_list[0]
+                        index_of_second_symbol_in_diff = self.key_symbols[symbol_list[0]].index
+                    break
+
+            if diff is None:
+                print("Cannot find difference with 2 variables.")
+                return
+            
+            print("Difference with 2 terms:", free_symbols, " Difference:", diff)
+            M = sp.Matrix(self.div, 1, lambda row, col: sp.cancel(v_div_diff[row, 0] / diff)) # Can be optimized for performance, but require more complex code
+            print(f"v_div_diff / ({diff}): {M}")
+
+             # Assume if there are 3 columns, there will be still columns cannot be canceled out yet and they will still have three variables
+            row_index_with_3_symbols = None
+            if len(self.key_symbols) == 3:
+                for i in range(0, self.div):
+                    if len(M[i, 0].free_symbols) == 3:
+                        row_index_with_3_symbols = i
+                        break
+                if row_index_with_3_symbols is None:
+                    print("Cannot find remaining row with 3 variables.")
+                    return
+
+            #print(v_div_diff[self.key_symbols[key_symbols_keys[0]].index])
+            #print(len(M[self.key_symbols[key_symbols_keys[1]].index, 0].free_symbols))
+            assert v_div_diff[self.key_symbols[key_symbols_keys[0]].index] == 0
+            assert len(M[index_of_second_symbol_in_diff, 0].free_symbols)==0
+            
+            i = len(self.key_symbols[key_symbols_keys[0]].members) # occurence of c_0
+            j = self.v_div[self.key_symbols[key_symbols_keys[0]].index] - self.v_div[index_of_second_symbol_in_diff] # difference c_0 - c_i as n == d
+            k = M[index_of_second_symbol_in_diff, 0] # factor of difference from previous step to current step
+            #c_i_times = len(self.key_symbols[key_symbols_keys[1]].members)
+            if len(self.key_symbols) == 2:
+                print(f"# Apply rule of \"1 difference with 2 terms\" (#2).")
+                print(f"i: {i} # occurences of c_0 = mu_0; occurences of c_1 = mu_1")
+                mu_0 = i
+                mu_1 = self.div - mu_0
+                print(f"j: {j} # difference c_0 - c_i as n = d")
+                print(f"k: {k} # factor of difference from previous step to current step")
+                d = self.div
+                n = d   # for example
+                print(f"# Formula for {key_symbols_keys[0]}: (2**n + (mu_1 * j * k**((n//d)-1)))// d")
+                print(f"# Formula for {key_symbols_keys[1]}:  (2**n - (i * j * k**((n//d)-1))// d")
+                formula2_0 = lambda n, d: (2**n + (mu_1 * j * k**((n//d)-1)))//d
+                formula2_1 = lambda n, d: (2**n - (i * j * k**((n//d)-1))) // d
+                print(f"# n = {n} => {key_symbols_keys[0]} = {formula2_0(n, d)}, {key_symbols_keys[1]} = {formula2_1(n, d)}")
+            else: # len(self.key_symbols) == 3:
+                print(f"i: {i} # occurences of c_0 = mu_0")
+                print(f"j: {j} # difference c_0 - c_i as n = d")
+                print(f"k: {k} # factor of difference from previous step to current step")
+                mu_0 = i 
+                mu_1 = len(self.key_symbols[self.sym_c[index_of_second_symbol_in_diff]].members)
+                mu_2 = len(self.key_symbols[self.sym_c[row_index_with_3_symbols]].members)
+                print(f"mu_1: {mu_1} # occurences of {self.sym_c[index_of_second_symbol_in_diff]}")
+                print(f"mu_2: {mu_2} # occurences of {self.sym_c[row_index_with_3_symbols]}")
+                o = v_div_diff[row_index_with_3_symbols, 0].coeff(key_symbols_keys[0])
+                p = v_div_diff[row_index_with_3_symbols, 0].coeff(self.sym_c[index_of_second_symbol_in_diff])
+                q = v_div_diff[row_index_with_3_symbols, 0].coeff(self.sym_c[row_index_with_3_symbols])
+                diff_0 = self.v_div[0]-self.v_div[row_index_with_3_symbols]
+                print(f"o (coefficient {key_symbols_keys[0]}): {o}")
+                print(f"p (coefficient {self.sym_c[index_of_second_symbol_in_diff]}): {p}")
+                print(f"q (coefficient {self.sym_c[row_index_with_3_symbols]}): {q}")
+                print(f"diff_0 ({key_symbols_keys[0]}-{self.sym_c[row_index_with_3_symbols]} as n = d): {diff_0}")
+                # assert coeff_c0 == coeff_c_in_diff    # Condition can be false!
+                assert (o + p + q) == 0
+                assert o > 0 and p > 0 and q < 0
+                #gcd = (-q).gcd(p).gcd(j).gcd(k)
+                #assert(gcd > 1)
+                #print(f"gcd(-q, p, j, k): {gcd}")
+                #assert ((-q) % k) == 0 and (p % k) == 0 and (j % k) == 0 # Condition can be false!
+                #print(f"m=1 =>  (-q) - p * j * k**(m-1): { (-q) - p * j * k**(0-1)}, c0-c1: {v_div[0]-v_div[1]}")
+                # REMARK: type of e.g. t in "t = 2**2000" -> <class 'int'>
+                # REMARK: type of e.g. p -> <class 'sympy.core.numbers.Integer'>                
+                # TODO CKP: try perfect_power, e.g. perfect_power(p)
+                #assert(power_of_two(p)) # TypeError: unsupported operand type(s) for &: ...
+                #assert(power_of_two(k)) # TypeError: unsupported operand type(s) for &: ...
+                # TODO CKP: try integer_log, e.g. integer_log(-q, 2)
+                assert(sp.log(p, 2) >= sp.log(k, 2))   
+                
+                formula3_part1 = lambda n, d: j * k**((n//d)-1)
+                # formula3_part2 = lambda n, d: self.v_div[row_index_with_3_symbols]
+                formula3_part2 = lambda n, d: diff_0 * (-q)**((n//d)-1) - p * j * (k**((n//d)-2)) * (((2**(sp.log(-q, 2) - sp.log(k, 2)))**((n//d)-1)) - 1) // (2**(sp.log(-q, 2) - sp.log(k, 2)) - 1)
+                print(f"# Apply rule of \"1 difference with 2 terms and a third term with this difference and a third variable\" (#3).")
+                print("difference c0 - c of row_index_of_second_symbol_in_diff:")
+                print("# f3_1(n, d): j * k**((n//d)-1)")
+                print("difference c0 - c of row_index_with_3_symbols: ")
+                #print(" m = 1: self.v_div[row_index_with_3_symbols]")
+                print("# f3_2(n, d): m >= 1: j * (-q)**((n//d)-1) - p * j * (k**((n//self.div)-2)) * (s from 0 to (m-2) Sum of ((2**(log_2(p) - log_2(k))**s)")
+                print("  => diff_0 * (-q)**((n//d)-1) - p * j * (k**((n//self.div)-2)) * ((2**(log_2(-q) - log_2(k))**s) - 1) // (2**(log_2(-q) - log_2(k)) - 1)")                
+                d = self.div
+                # Formulars for coefficients
+                print(f"# Formula for {key_symbols_keys[0]}: (2**n + mu_1 *  f3_1(n, d) + mu_2 * f3_2(n, d))// d")
+                print(f"# Formula for {self.sym_c[index_of_second_symbol_in_diff]}: (2**n + (mu_1 - d) *  f3_1(n, d) + mu_2 * f3_2(n, d))// d")
+                print(f"# Formula for {self.sym_c[row_index_with_3_symbols]}: (2**n + mu_1 * f3_1(n, d) + (mu_2 - d) * f3_2(n, d))// d")
+                formula3_0 = lambda n, d: (2**n + mu_1 * formula3_part1(n, d) + mu_2 * formula3_part2(n, d)) // d
+                formula3_1 = lambda n, d: (2**n + (mu_1 - d) * formula3_part1(n, d) + mu_2 * formula3_part2(n, d)) // d
+                formula3_2 = lambda n, d: (2**n + mu_1 * formula3_part1(n, d) + (mu_2 - d) * formula3_part2(n, d)) // d                
+                n = d # for example
+                print(f"# n = {n} => {key_symbols_keys[0]} = {formula3_0(n, d)}, {self.sym_c[index_of_second_symbol_in_diff]} = {formula3_1(n, d)}, {self.sym_c[row_index_with_3_symbols]} = {formula3_2(n, d)}")
+                
+                # Tests
+                for ni in range(1, 6):
+                    n = ni * d
+                    print(f"# n = {n} => c0-{self.sym_c[index_of_second_symbol_in_diff]}: {formula3_part1(n, d)}, c0-{self.sym_c[row_index_with_3_symbols]}: {formula3_part2(n, d)}")
+
+                M = create_matrix_from_solution_vector_sp(self.v_div)
+                v_new = self.v_div
+                for var in range(1, 6):
+                    print(f"(matric calc.) m: {var}, c0-c1: {v_new[0]-v_new[1]}, c0-{self.sym_c[index_of_second_symbol_in_diff]}: {v_new[0]-v_new[index_of_second_symbol_in_diff]}")
+                    v_new = M * self.v_div
+                    M = create_matrix_from_solution_vector_sp(v_new)
+
+            #    #<expr>.atoms(Symbol) 
+            #    temp, with_ci = v_div_diff[i, 0].as_independent((diff), as_Mul=True)
+            #    print("temp, with_ci: ", temp, with_ci)
+         # Filter rule I'm working on:
+        else: # i.e. len(self.key_symbols) > 3:
+            raise NotImplementedError(f"## Ignore div={self.div} for now. Key symbols count {len(self.key_symbols)}.")
+            
+
+
+## test sympy symbols
+def test_symbols(div):
+    """Construct formulas for current problem"""
+    symbolTable = SymbolTable(div)
+    v_div = symbolTable.get_v_div()
+    M = symbolTable.return_symbol_matrix()
+    
+    r = M * v_div
+    v_div_diff = sp.Matrix(div, 1, lambda row, col: r[0, 0] - r[row, 0])
+    print("##### div:", div)                
+    print("# v_div: ", v_div)
+    print("# v_div_diff: ", v_div_diff)
+    print("# r = M * v_div: ", r)
+    #print("# M.det(): ", M.det())
+    try:
+        symbolTable.calculate_result(v_div_diff)
+    except NotImplementedError as nie:
+        print(str(nie))
+    print("")
+
+def test_symbols_2(div): # print vectors only as symbols
+    sym_c = symbols('c:' + str(div))
+    v = sp.Matrix(div, 1, lambda row, col: sym_c[row] )
+
+    vs = []
+    vs.append(v)
+    for i in range(0, 4):
+        M_i = create_matrix_from_solution_vector_sp(vs[i])
+        v_new = M_i * v
+        vs.append(v_new)
+    
+    print("##### div:", div)
+    for i in range(0, 5):                
+        print("# i:", i, "vs[i]", vs[i])
+
+def test_symbols_3(div): # print vectors as multiples of initial symbols
+    sym_c = symbols('c:' + str(div))
+    v = sp.Matrix(div, 1, lambda row, col: sym_c[row] )
+    v_num = generate_solution_vectors_sp(div)[div-1]
+
+    vs = []
+    vs.append(v)
+    for i in range(0, 4):
+        M_i = create_matrix_from_solution_vector_sp(vs[i])
+        vs_new = M_i * v_num
+        vs.append(vs_new)
+    
+    print("##### div:", div)
+    for i in range(0, 5):                
+        print("# i:", i, "vs[i]", vs[i])
+
+def test_symbols_4():
+    symbol_count_to_div = {}
+    for div in range(2, 101):
+        symbolTable = SymbolTable(div)
+
+        count = len(symbolTable.key_symbols)
+        if count in symbol_count_to_div:
+            symbol_count_to_div[count].append(div)
+        else:
+            symbol_count_to_div[count] = [div]
+
+    print(f"symbol_count_to_div: {symbol_count_to_div}")
+
+
+def main():
+    init_printing(use_unicode=False, wrap_line=False, num_columns=300)
+
+    for i in range(2, 101):
+        test_symbols(i)
+    #test_symbols_2(5)
+    #test_symbols_3(15)
+
+    #test_symbols_4()
+
+
+if __name__ == '__main__':
+    main()
+
